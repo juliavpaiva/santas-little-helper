7dcb5185b40c4c8286be3894ee353890d727fca6
Julia Paiva <juliavpaiva@outlook.com>
2022-11-02 16:31:42 -0300

    Adding Sign Up

--- santas-little-helper-app/src/App.tsx
+++ santas-little-helper-app/src/App.tsx
@@ -1,10 +1,11 @@
 import * as React from 'react';
 import Button from '@mui/material/Button';
-import UserSignIn from './components/sign/sign_in/user_signin';
-import GuestChooseUser from './components/sign/sign_in/guest/guest_choose_user';
+import UserSignIn from './components/sign/sign-in/user_signin';
+import GuestChooseUser from './components/sign/sign-in/guest/guest_choose_user';
+import SignUp from './components/sign/sign-up/sign_up';
 
 function App() {
-  return <UserSignIn/>
+  return <SignUp/>
 }
 
 export default App;
